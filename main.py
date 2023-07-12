1e2abb6ebd8763096c05ca877d4ebf20f21fa414
Roxana Olaru <roxana.olaru14@gmail.com>
2023-07-13 00:48:50 +0300

    Counting students by countries

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
 
-#creare Spark Session
+# Creare Spark Session
 spark = SparkSession.builder.master("local[1]")\
                     .appName('Spark1')\
                     .getOrCreate()
 
-#citire fisier Erasmus.csv intr-un DataFrame
+# Citire fisier Erasmus.csv intr-un DataFrame
 df = spark.read.options(header='True', inferSchema='True') \
             .csv("C:/Users/user/Desktop/IBM practica/Erasmus.csv")
 
-#afisare continut (fara a trunchia)
-df.show(truncate=False)
+# Afisare continut initial (fara a trunchia)
+# df.show(truncate=False)
 
+# Grupare studenti in functie de codurile tarilor, numararea acestora pentru fiecare caz si ordonarea crescatoare dupa coduri
+df2 = df.groupby("Receiving Country Code", "Sending Country Code") \
+                .agg(count("*")) \
+                .orderBy("Receiving Country Code", "Sending Country Code")
 
+# Afisare finala
+df2.show(df.count())
+
+# Verificare numar de randuri (in acest caz 3184 - coincide cu cel din Excel)
+print(df.count())
