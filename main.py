311025b303721ad96d411bef3b36d4f5723b3364
Roxana Olaru <roxana.olaru14@gmail.com>
2023-07-13 17:55:13 +0300

    Filter for countries (before groupBy)

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ df = spark.read.options(header='True', inferSchema='True') \
 # Afisare continut initial (fara a trunchia)
 # df.show(truncate=False)
 
+# Filtrare dupa coduri tari (daca sunt LV MK MT)
+df_filtrat = df.filter(df['Receiving Country Code'].isin('LV', 'MK', 'MT'))
+
 # Grupare studenti in functie de codurile tarilor, numararea acestora pentru fiecare caz si ordonarea crescatoare dupa coduri
-df2 = df.groupBy("Receiving Country Code", "Sending Country Code") \
+df2 = df_filtrat.groupBy("Receiving Country Code", "Sending Country Code") \
                 .agg(count("*")) \
                 .orderBy("Receiving Country Code", "Sending Country Code")
 
-# Filtrare dupa coduri tari (daca sunt LV MK MT)
-df2_filtrat = df2.filter(df2['Receiving Country Code'].isin('LV', 'MK', 'MT'))
 
 # Afisare finala
-df2_filtrat.show(df2_filtrat.count())
+df2.show(df2.count())
