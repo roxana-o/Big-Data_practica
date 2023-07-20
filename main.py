69f9f128857d7fc77467a12bed592958191bd70d
Roxana Olaru <roxana.olaru14@gmail.com>
2023-07-20 16:58:35 +0300

    Export to MySQL

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count
+import findspark
+findspark.add_packages('mysql:mysql-connector-java:8.0.11')
 
 # Creare Spark Session
 spark = SparkSession.builder.master("local[1]")\
@@ -14,13 +16,25 @@ df = spark.read.options(header='True', inferSchema='True') \
 # df.show(truncate=False)
 
 # Filtrare dupa coduri tari (daca sunt LV MK MT)
-df_filtrat = df.filter(df['Receiving Country Code'].isin('LV', 'MK', 'MT'))
+df_filtered = df.filter(df['Receiving Country Code'].isin('LV', 'MK', 'MT'))
 
 # Grupare studenti in functie de codurile tarilor, numararea acestora pentru fiecare caz si ordonarea crescatoare dupa coduri
-df2 = df_filtrat.groupBy("Receiving Country Code", "Sending Country Code") \
+df2 = df_filtered.groupBy("Receiving Country Code", "Sending Country Code") \
                 .agg(count("*")) \
                 .orderBy("Receiving Country Code", "Sending Country Code")
 
+# Configuratii conectare la baza de date MySQL
+properties = {
+    "user": "root",
+    "password": "root",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+# Exportare date in 3 tabele separate in functie de tara (LV, MK, MT)
+countries = ["LV", "MK", "MT"]
+for country in countries:
+    df3 = df2.filter(df2['Receiving Country Code'] == country)
+    df3.write.jdbc("jdbc:mysql://localhost:3306/erasmus?useSSL=false", "Erasmus_" + country, "overwrite", properties)
 
 # Afisare finala
 df2.show(df2.count())
